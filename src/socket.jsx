4d92389460e90508b459e32ff3a7f84cc61bd075
avisekh3110 <avisekh.sabi31102003@gmail.com>
2025-11-09 18:54:27 +0530

    socket test

--- src/socket.jsx
+++ src/socket.jsx
@@ -4,7 +4,4 @@ import { LocalPort } from "./const.js";
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = `${LocalPort}`;
 
-export const socket = io(URL, {
-  transports: ["websocket"], // optional: force WS only
-  withCredentials: true,
-});
+export const socket = io(URL);
